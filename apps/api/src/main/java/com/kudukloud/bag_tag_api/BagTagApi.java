1126bcc45b6ba4dc7cb3e8387219a688bed3f8e3
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2026-03-12 19:00:05 +0100

    Rename main application and test classes to `BagTagApi` and update project JDK configuration

--- apps/api/src/main/java/com/kudukloud/bag_tag_api/BagTagApi.java
+++ apps/api/src/main/java/com/kudukloud/bag_tag_api/BagTagApi.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BagTagApiApplication {
+public class BagTagApi {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BagTagApiApplication.class, args);
