507c4fddbd95f4399654a46c29be3ae0eda29ea9
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2026-03-12 21:29:06 +0100

    Add `StartupLogger` component to log application startup events

---
+++ apps/api/src/main/kotlin/com/kudukloud/bagtag/api/common/StartupLogger.kt
@@ -0,0 +1,27 @@
+package com.kudukloud.bagtag.api.common
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class StartupLogger {
+  private val logger = LoggerFactory.getLogger(javaClass)
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun logApplicationReady() {
+    logger.info(STARTUP_BANNER)
+  }
+
+  companion object {
+    private val STARTUP_BANNER =
+        """
+
+        === ------------------------------ ===
+        === BagTag API Application Started ===
+        === ------------------------------ ===
+        """
+            .trimIndent()
+  }
+}
